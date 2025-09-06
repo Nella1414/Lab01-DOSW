8fccaebb4571a113c827fbf5a1f9475d4940fb3e
Nella1414 <marianella.polo-p@mail.escuelaing.edu.co>
2025-09-06 00:32:25 -0500

    Reto 6: final commit

--- Reto-6/src/MaquinaMisteriosa.java
+++ Reto-6/src/MaquinaMisteriosa.java
@@ -7,6 +7,10 @@ public class MaquinaMisteriosa {
         comandos.put("DESPEDIR", () -> System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión."));
         comandos.put("CANTAR", () -> System.out.println("La máquina canta: 01010101"));
         comandos.put("DANZAR", () -> System.out.println("La máquina gira y emite chispas: Girando en modo fiesta."));
+        comandos.put("BROMEAR", () -> System.out.println("La máquina ríe: ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio..."));
+        comandos.put("GRITAR", () -> System.out.println("La máquina grita: ¡¡¡ALERTA DE STACK OVERFLOW!!!"));
+        comandos.put("SUSURRAR", () -> System.out.println("La máquina susurra: Shhh... los bugs están dormidos."));
+        comandos.put("ANALIZAR", () -> System.out.println("La máquina procesa: Analizando datos... resultado: ¡Eres increíble programando!"));
     }
     public static void ejecutarComando(String comando) {
         switch (comando) {
@@ -14,6 +18,10 @@ public class MaquinaMisteriosa {
             case "DESPEDIR":
             case "CANTAR":
             case "DANZAR":
+            case "BROMEAR":
+            case "GRITAR":
+            case "SUSURRAR":
+            case "ANALIZAR":
                 comandos.get(comando).run();
                 break;
             default:
@@ -25,6 +33,10 @@ public class MaquinaMisteriosa {
         ejecutarComando("BROMEAR");
         ejecutarComando("ANALIZAR");
         ejecutarComando("DANZAR");
+        ejecutarComando("GRITAR");
+        ejecutarComando("SUSURRAR");
+        ejecutarComando("CANTAR");
+        ejecutarComando("DESPEDIR");
         ejecutarComando("INVALIDO"); // caso extra
     }
 }
