f0ea2b4c87da9011f3549fd9dcc3908bab7c3a4a
Carlos Piedrahita <carlos8piedrahita@gmail.com>
2025-09-05 21:17:41 -0500

    Reto 2: primera vuelta

---
+++ Reto-2/src/Resultados.java
@@ -0,0 +1,11 @@
+package com.lab.retos;
+
+/** DTO inmutable de resultados del Reto 2 */
+public record Resultados(
+        int maximo,
+) {
+    @Override public String toString() {
+        return "Resultados{" +
+                "max=" + maximo +
+    }
+}
